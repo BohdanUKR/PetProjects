96b75380b43c1e304719355f4c856697d5b23a94
Bohdan Drozd <drozd.bogdan35@gmail.com>
2022-10-01 23:53:19 -0400

    Commit About

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,12 @@ router.get('/', function(req, res, next)
 //Get About
 router.get('/partial/about', function(req,res,next)
 {
-  res.render('partial/about' , {title: 'About Me'});
+   res.render('partial/about');
+});
+//Get Services
+router.get('/partial/services',function(req,res,next)
+{
+  res.render('partial/services');
 });
 
 module.exports = router;
