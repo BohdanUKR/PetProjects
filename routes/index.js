468232348aabaf6c3f7d481b51d6c2ead4bc620a
Bohdan Drozd <drozd.bogdan35@gmail.com>
2022-10-03 00:48:28 -0400

    Added Contact Page

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,23 @@ router.get('/', function(req, res, next)
 {
    res.render('index');
 });
+
 //Get About
 router.get('/about', function(req,res,next)
 {
    res.render('about');
 });
+
 //Get Services
 router.get('/services',function(req,res,next)
 {
   res.render('services');
+});
+
+//Get Contact
+router.get('/contact',function(req,res,next)
+{
+   res.render('contact');
 });
 
 module.exports = router;
